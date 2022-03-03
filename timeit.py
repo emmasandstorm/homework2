1cb016eff51440a06695d51915cc4c867dc4b002
Emma <emma@example.com>
2022-03-02 22:47:47 -0800

    finished timeit.py

---
+++ timeit.py
@@ -0,0 +1,9 @@
+import time
+
+def calculate_time(time):
+	time_start = time()
+	time_end = time()
+	total_time = time_end - time_start
+	print ('Total time', total_time)
+
+calculate_time(time.time)
