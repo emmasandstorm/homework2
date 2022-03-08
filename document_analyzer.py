1f69d0767c2b82d0dbb00bbad5c861ef94e4d006
Emma <emma@example.com>
2022-03-07 20:53:20 -0800

    v2 of analyzer

--- document_analyzer.py
+++ document_analyzer.py
@@ -6,17 +6,9 @@ def word_count(fname):
 		top_5 = counted_dict.most_common(5)
 		for word, count in top_5:
 			print(word,":", count)
+	return counted_dict.most_common(5)
 
 word_count("document.txt")
 
 
-
-
-
-file = open('document.txt', 'r')
-read_data = file.read()
-per_word = read_data.split()
-
-
-print("Total words:", len(per_word))
 
