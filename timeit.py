8ed612e244fbd0d8437bfe9c69eb92c3a3de8fc0
Emma <emma@example.com>
2022-03-08 14:27:47 -0800

    v8 of timeit.py

--- timeit.py
+++ timeit.py
@@ -1,15 +1,18 @@
 import time
 
 def calculate_time(func):
-    def wrapper():
-        time_start = time.time()
-        time_end = time.time()
-        total_time = time_end - time_start
-        print ('Total time', total_time)
-    return wrapper
+	def wrapper():
+        	time_start = time.time()
+		func()
+		time_end = time.time()
+		total_time = time_end - time_start
+		print ('Total time', total_time)
+	return wrapper
 
 @calculate_time
-def print_time():
-    print ('Total time')
-    
-print_time()
+def function():
+	n = 0
+	while n < 1000:
+		n +=1
+
+function()
