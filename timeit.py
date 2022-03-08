e72e1efcaedd9a05a72af613f60640c8539dea88
Emma <emma@example.com>
2022-03-07 21:09:43 -0800

    v4 of timeit.py

--- timeit.py
+++ timeit.py
@@ -1,8 +1,8 @@
 import time
 
 def calculate_time(time):
-	time_start = int(time())
-	time_end = int(time())
+	time_start = time()
+	time_end = time()
 	total_time = time_end - time_start
 	print ('Total time', total_time)
 
